87a843b6dace930d209dce6b0fc107106f7ba44a
Frédéric Van Overmeire <frederic.vanovermeire@gmail.com>
2021-09-27 16:31:25 +0200

    Finished

--- Ex4/4-4.js
+++ Ex4/4-4.js
@@ -1,9 +1,67 @@
 const readlineSync = require("readline-sync");
 
-let i = readlineSync.question('Please enter a number: ')
 
 
-function average(arr) {
-    const arr = Array.from ({length: i})
-    console.log (arr/arr.length)
+
+/**
+ * Gives a number between 1 and 10
+ * @author Frédéric Van Overmeire
+ */
+function rand10() {
+    return Math.floor((Math.random()*10)+1);
 }
+
+/**
+ * Input a number to get an array of random number
+ * @author Frédéric Van Overmeire
+ * @param {Array} n 
+ */
+function multiRand(n) {
+    var n = Array.from({length: b}, () => Math.floor(rand10()));
+    return n;
+}
+
+let b = readlineSync.question('Please enter a number: ')
+
+/**
+ * Calculate the average of a given array
+ * @author Frédéric
+ * @param {array} arr 
+ * @returns average
+ */
+function average(arr) {
+    let a = 0;
+    for (i=0; i != arr.length; i++) {
+    a += arr[i] ;
+}
+    return a / arr.length;
+}
+
+/**
+ * Gives the minimum of an array
+ * @author Frédéric
+ * @param {array} arr 
+ * @returns minimum
+ */
+function min(arr) {
+    return Math.min (...arr)
+    
+}
+
+/**
+ * Gives the maximum of an array
+ * @author Frédéric
+ * @param {array} arr 
+ * @returns maximum
+ */
+function max(arr) {
+    return Math.max (...arr)
+}
+
+let myArr = multiRand(b);
+
+
+console.log(`Your array is : [${myArr}]`);
+console.log(`The average is : ${average(myArr)}`);
+console.log(`The minimum is : ${min(myArr)}`);
+console.log(`The maximum is : ${max(myArr)}`);
